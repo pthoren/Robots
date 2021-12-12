e9292904d3ecc6e4c4773ed83931b2ec8c4fab31
Paul Thoren <pthoren@palantir.com>
2021-12-11 22:44:26 -0500

    more tests

--- Robots/Controllers/RobotsController.cs
+++ Robots/Controllers/RobotsController.cs
@@ -17,7 +17,10 @@ namespace Robots.Controllers
         public RobotsController(ILogger<RobotsController> logger)
         {
             this.log = log;
-            this.robotService = new RobotService(new List<string> { "https://60c8ed887dafc90017ffbd56.mockapi.io/robots" });
+            this.robotService = new RobotService(new List<string> {
+                "https://60c8ed887dafc90017ffbd56.mockapi.io/robots",
+                "https://svtrobotics.free.beeceptor.com/robots"
+            });
         }
 
         [HttpPost]
